aba23ab844fe6962825d58daa44c365a1a71d050
PMT201 <PMT201@PMT3044>
2026-03-06 09:16:59 +0700

    bai1

--- KyThuatLapTrinh/Buoi1/buoi1.cpp
+++ KyThuatLapTrinh/Buoi1/buoi1.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
-
+	vector <float> diemthi;
+	diemthi.push_back(10);
+	diemthi.push_back(4);
+	diemthi.push_back(6);
+	diemthi.pop_back();
+	for (int i = 0; i < diemthi.size(); i++) {
+		cout << diemthi[i] << endl;
+	}
+	
 }
